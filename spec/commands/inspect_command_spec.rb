893a310f62a1fb23afbda9d0e56aa6a6c7ca8bbc
Jason Frey <fryguy9@gmail.com>
2011-04-21 13:51:02 -0400

    Fixed tests to work on Windows.

--- spec/commands/inspect_command_spec.rb
+++ spec/commands/inspect_command_spec.rb
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__), "..", "spec_helper")
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "spec_helper"))
 
 describe InspectCommand do
   it "displays information about the object you specify" do
